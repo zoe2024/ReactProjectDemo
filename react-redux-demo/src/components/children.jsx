338aad16ad8273271545701599bd864cfc69d2a9
zoe <yuan.zhou@zznode.com>
2021-10-12 23:50:07 +0800

    redux练习（已完成）

--- react-redux-demo/src/components/children.jsx
+++ react-redux-demo/src/components/children.jsx
@@ -1,16 +1,15 @@
 /*
  * @Date: 2021-10-11 23:39:08
- * @Descripton: 
- * @LastEditTime: 2021-10-12 00:21:57
+ * @Descripton: reducx 练习
+ * @LastEditTime: 2021-10-12 23:47:24
  */
 import React from "react";
 export default function (props) {
-  const reducer = () => {}
   return (
     <div className="container">
-      <button onClick={() => { }}>add</button>
+      <button onClick={props.add}>add</button>
       <span> &nbsp;&nbsp;&nbsp; </span>
-      <button onClick={() => { }}>dec</button>
+      <button onClick={props.dec}>dec</button>
     </div>
   )
 }
--- react-redux-demo/src/components/parent.jsx
+++ react-redux-demo/src/components/parent.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Children from "./children";
+import { createStore } from 'redux';
+import reducer from "../store/reducer";
+
+const store = createStore(reducer)
+
 export default function () {
+  const [count, setCount] = useState(store.getState())
+  const unSub = store.subscribe(() => {
+    setCount(store.getState())
+  })
+  useEffect(() => {
+    return () => {
+      unSub()
+    }
+  })
   return (
     <div className="container">
-      parent-count: 0
-      <Children />
+      parent-count: {count}
+      <Children 
+      add={() => store.dispatch({ type: "ADD" })} 
+      dec={() => store.dispatch({ type: "DEC" })} />
     </div>
   )
 }
