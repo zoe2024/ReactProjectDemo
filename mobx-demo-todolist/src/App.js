8115da679f4f28c11058bd0a95217739c8400079
zoe <yuan.zhou@zznode.com>
2021-10-19 23:49:49 +0800

    练习2

--- mobx-demo-todolist/src/App.js
+++ mobx-demo-todolist/src/App.js
@@ -1,12 +1,12 @@
 /*
  * @Date: 2021-10-17 22:14:14
  * @Descripton: 
- * @LastEditTime: 2021-10-17 22:31:45
+ * @LastEditTime: 2021-10-19 23:03:11
  */
 import React, { Component } from 'react';
 import './App.css';
-import TodoListStore from "./components/TodoListStore";
-import TodoListView from "./components/TodoListView";
+import TodoListStore from "./components/TodoListStore2";
+import TodoListView from "./components/TodoListView2";
 import { Provider } from 'mobx-react';
 
 export default class App extends Component {
---
+++ mobx-demo-todolist/src/components/TodoListView2.jsx
@@ -0,0 +1,54 @@
+import React, { Fragment } from "react";
+import { inject, observer } from "mobx-react";
+
+@inject("TodoListStore")
+@observer
+class TodoListView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.store = this.props.TodoListStore;
+  }
+  state = {
+    todoTitle: ""
+  }
+  handelTodoTitleChnage = (ev) => {
+    this.setState({ todoTitle: ev.target.value })
+  }
+  submit = (ev) => {
+    ev.preventDefault()
+    console.log(this.state.todoTitle);
+    this.store.addTodo(this.state.todoTitle)
+    this.setState({ todoTitle: "" })
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <form onSubmit={this.submit}>
+            <input type="text" name="ipt" id="ipt" onChange={this.handelTodoTitleChnage} value={this.state.todoTitle} autoFocus />
+          </form>
+        </header>
+        <main>
+          <ul>
+            {this.store.todos.map(todo => (
+              <Fragment key={todo.id}>
+                <li>
+                  <input type="checkbox" checked={todo.isFinshed} onChange={() => todo.isFinshed = !todo.isFinshed} />
+                  {
+                    todo.isFinshed ? <del>{todo.title}</del> : <span>{todo.title}</span>
+                  }
+                  <span style={{ marginLeft: "16px", cursor: "pointer" }} onClick={() => this.store.removeTodo(todo)}>X</span>
+                </li>
+              </Fragment>
+            ))}
+          </ul>
+        </main>
+        <footer>
+          <span>{this.store.left} todo(s) is unFinshed</span>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default TodoListView
